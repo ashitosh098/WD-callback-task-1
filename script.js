c1c83586d33de38aeef0f7a6354c8505d07ace6c
ashitosh098 <61306088+ashitosh098@users.noreply.github.com>
2021-04-07 20:02:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var countdown = document.createElement('div')
+countdown.style.fontSize = '50px';
+countdown.style.margin='50px 30%';
+countdown.style.textAlign='center';
+document.body.append(countdown);
+countdown.innerText=10;
+
+
+
+
+
+
+
+setTimeout(() => {
+    countdown.innerText=9;
+    setTimeout(() => {
+        countdown.innerText=8;
+        setTimeout(() => {
+            countdown.innerText=7;
+            setTimeout(() => {
+                countdown.innerText=6;
+                setTimeout(() => {
+                    countdown.innerText=5;
+                    setTimeout(() => {
+                        countdown.innerText=4;
+                        setTimeout(() => {
+                            countdown.innerText=3;
+                            setTimeout(() => {
+                                countdown.innerText=2;
+                                setTimeout(() => {
+                                    countdown.innerText=1;
+                                    setTimeout(() => {
+                                        countdown.innerText='happy independence day';
+
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
